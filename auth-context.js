f6812a07dd93349edef2a489c63f97c68704726b
krutika <137177323+uygsj@users.noreply.github.com>
2023-09-01 16:52:42 +0530

    Add files via upload

--- auth-context.js
+++ auth-context.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
-})
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
 
-export default AuthContext
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storeUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storeUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn', '1')
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
+    setIsLoggedIn(false);
+  };
+  
+
+  return ( 
+    <AuthContext.Provider 
+      value={{
+        isLoggedIn: isLoggedIn, 
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContext;
